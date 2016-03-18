2cbd855e9d0ff2e631d982efa7049cdff043f338
Ke Er (Brenda) Xiong <krx.sky@gmail.com>
2016-03-18 10:06:17 -0700

    go fmt

--- network/network.go
+++ network/network.go
@@ -2,43 +2,42 @@
 // broadcasting, and any passing of info
 // among nodes
 
-// Set-up: 
+// Set-up:
 // $ go get github.com/satori/go.uuid
 
 package network
 
-import(
+import (
 	"../util"
-	"net"
 	"github.com/satori/go.uuid"
+	"net"
 )
 
 // data structures
 
 type Node struct {
-	Addr string
-	Quitted bool  // node is forever quitted if it voluntarily leave the network
+	Addr    string
+	Quitted bool // node is forever quitted if it voluntarily leave the network
 
 	// fields below are for local communication purpose
-	connected bool // true if current node is connected to target node
-	in   *net.TCPConn // tcpConn for listening from that node
-	out  *net.TCPConn // tcpConn for writing to that node
+	connected bool         // true if current node is connected to target node
+	in        *net.TCPConn // tcpConn for listening from that node
+	out       *net.TCPConn // tcpConn for writing to that node
 }
 
-type NodeMetaData = struct {
-	Id    string
-	Addr  string
+type NodeMetaData struct {
+	Id   string
+	Addr string
 }
 
-type NodeMap = struct {
+type NodeMap struct {
 	sync.RWMutex
 	Nodes map[string]Node
 }
 
 // global variables
-var nodeMetaData NodeMetaData // keeps track of current node's information
+var nodeMetaData NodeMetaData                       // keeps track of current node's information
 var nodeMap = NodeMap{Nodes: make(map[string]Node)} // keeps track of all nodes in the system
-
 
 // listen for incoming connection
 func listenForConn(listener *TCPListener) {
@@ -50,30 +49,27 @@ func listenForConn(listener *TCPListener) {
 
 // handle incoming connection
 func handleConn(conn net.Conn) {
-	// TODO: read incoming messages. 
+	// TODO: read incoming messages.
 	// Deal with following cases:
 
-
-	// new node joining : 
+	// new node joining :
 	// send saved nodeMap to that node, add that node to nodeMap, merge treedoc if necessary.
 
 	// known node quitting :
 	// set map[node].Quitted = true.
 
-
 	// editing treedoc:
 	// modify treedoc
-
 
 	// ** for any received messages, broadcast may be required depending on the situation
 }
 
-// initialize local network listener 
+// initialize local network listener
 func Initialize(addr string) error {
 	lAddr, err := net.ResolveTCPAddr("tcp", addr)
 	listener, err := net.ListenTCP("tcp", lAddr)
 
-	if err == nil{ 
+	if err == nil {
 		newUUID := uuid.NewV1().String()
 		nodeMataData = NodeMetaData{newUUID, lAddr}
 		go listenForConn(listener)
