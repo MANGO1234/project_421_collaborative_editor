f6d11a73a9e7a525901768089f0b1fdb3f3fa444
Emma Zhang <emmazhang211@hotmail.com>
2016-03-18 17:47:21 -0700

    fix error and compile

--- network/network.go
+++ network/network.go
@@ -40,8 +40,6 @@ type NodeMetaData struct {
 	NodeMap map[string]Node
 }
 
-var mapLock *sync.RWMutex
-
 // global variables
 var nodeMetaData = NodeMetaData{NodeMap: make(map[string]Node)} // keeps track of nodeMetaData
 var mapLock *sync.RWMutex
@@ -66,19 +64,27 @@ func handleConn(conn net.Conn) {
 	// send saved nodeMap to that node, add that node to nodeMap, merge treedoc if necessary.
 	msgWriter := util.MessageWriter{bufio.NewWriter(conn)}
 	msgReader := util.MessageReader{bufio.NewReader(conn)}
-	msgInType, msgIn, err := msgReader.ReadMessage2()
-	connWrapper := ConnWrapper{&ConnReader: msgReader, &ConnWriter: msgWriter}
-	util.CheckError(err)
+	msgInType, m, err := msgReader.ReadMessage2()
+	handleError(err)
 
-	fmt.Println("received message: ", string(msgIn), "asType: ", msgInType) //
+	var msgIn NodeMetaData
+	err = json.Unmarshal(m, &msgIn)
+
+	fmt.Println("received message Type: ", msgInType) //
 	//add this node to nodeMap
-	mapLock.lock()
-	if nodeMetaData.NodeMap[msgIn.Id] == nil {
-		nodeMetaData.NodeMap[msgIn.Id] = Node{Addr: msgIn.Addr, Quitted: false, connected: true, in: connWrapper}
+	mapLock.Lock()
+	_, ok := nodeMetaData.NodeMap[msgIn.Id]
+	if !ok {
+		nodeMetaData.NodeMap[msgIn.Id] = Node{Addr: msgIn.Addr, Quitted: false, connected: true}
 	}
 	mapLock.Unlock()
 
-	ftm.Println("send info: ", string(msgOut), "ofType: ", msgOutType)
+	// reply
+	if msgInType == RegMsg {
+		msg, _ := json.Marshal(nodeMetaData)
+		msgWriter.WriteMessage2(RegMsg, msg)
+	}
+	// TODO: connect back to node
 
 	// known node quitting :
 	// set map[node].Quitted = true.
@@ -97,7 +103,7 @@ func Initialize(addr string) error {
 
 	if err == nil {
 		newUUID := uuid.NewV1().String()
-		nodeMetaData = NodeMetaData{newUUID, lAddr.String()}
+		nodeMetaData = NodeMetaData{newUUID, lAddr.String(), make(map[string]Node)}
 		go listenForConn(listener)
 	}
 
@@ -161,6 +167,6 @@ func handleNewNodes(receivedNodeMap map[string]Node) {
 	}
 }
 
-func handleError() {
+func handleError(error error) {
 
 }
